3ce912cc1c689b0c55700d1b9c440c893e54959e
jasiekrr <janromanczuk99@gmail.com>
2022-06-20 18:09:16 +0200

    connection to API done

--- src/main/java/com/example/edpapp/api/WeatherApiService.java
+++ src/main/java/com/example/edpapp/api/WeatherApiService.java
@@ -2,12 +2,9 @@ package com.example.edpapp.api;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface WeatherApiService {
-    //@Headers("X-API-KEY: 947036533d519185ee295cc435beca63")
-    //@GET("data/2.5/weather")
-    @GET("?APPID=947036533d519185ee295cc435beca63&units=metric")
+   @GET("?APPID=&units=metric")
     Call<Weather> requestWeather(@Query("lat") double lat, @Query("lon") double lon);
 }
